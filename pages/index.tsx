2da0e01a798920867b6c1cdeb6a4b345746ae32f
rarepepi <pepimartinez@protonmail.com>
2023-02-09 03:41:39 -0500

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">
-        <div className="">
+      <main>
+        <div className="text-3xl font-bold underline">
           <h1>Welcome to the T2 template</h1>
         </div>
       </main>
